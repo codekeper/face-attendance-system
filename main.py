610695d27920af2fd9190bad61ec51d9c7a85626
Code Keper <aliswork1122@gmail.com>
2024-03-14 23:22:56 +0500

    face detection with square

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import cv2
+import numpy as np
+import face_recognition as fr
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+load_img = fr.load_image_file("dataset/abdul qadeer.jpg")
+rgb_img = cv2.cvtColor(load_img, cv2.COLOR_BGR2RGB)
 
+face_locations = fr.face_locations(rgb_img)[0]
+copy = rgb_img.copy()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+cv2.rectangle(copy, (face_locations[3], face_locations[0]),(face_locations[1], face_locations[2]), (255,0,255), 2)
 
+cv2.imshow('copy', copy)
+cv2.imshow('img', rgb_img)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+cv2.waitKey(0)
